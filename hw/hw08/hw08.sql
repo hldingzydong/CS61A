9cb8b7c4f81eda36a17a34f7accaa65afee016ab
847394047@qq.com <hy@hyLeedeMacBook-Pro.local>
2020-09-01 23:05:18 +0800

    hw08 done

--- hw/hw08/hw08.sql
+++ hw/hw08/hw08.sql
@@ -26,29 +26,45 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs.name, sizes.size FROM dogs, sizes WHERE dogs.height > sizes.min AND dogs.height <= sizes.max;
 
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT t1.name FROM parents, dogs AS t1, dogs AS t2 WHERE parents.child = t1.name AND parents.parent = t2.name ORDER BY t2.height DESC;
 
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT t1.name AS bro, t2.name AS lbro , size FROM dogs AS t1, dogs AS t2, sizes WHERE t1.height > sizes.min
+  AND t1.height <= sizes.max AND t2.height > sizes.min AND t2.height <= sizes.max AND t1.name < t2.name;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT bro || " and " || lbro || " are " || size || " siblings" FROM siblings, parents AS p1, parents AS p2
+  WHERE bro = p1.child AND lbro = p2.child AND p1.parent = p2.parent;
 
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
 CREATE TABLE stacks_helper(dogs, stack_height, last_height, n);
 
--- Add your INSERT INTOs here
+INSERT INTO stacks_helper SELECT dogs.name, dogs.height, dogs.height, 1 FROM dogs;
 
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+SELECT t1.dogs || ", " || t2.dogs, t1.stack_height + t2.stack_height, t2.last_height, 2
+FROM stacks_helper AS t1, stacks_helper AS t2 WHERE t1.last_height < t2.last_height;
+
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+SELECT t1.dogs || ", " || t2.dogs, t1.stack_height + t2.stack_height, t2.last_height, 3
+FROM stacks_helper AS t1, stacks_helper AS t2 WHERE t1.last_height < t2.last_height AND t1.n = 2 AND t2.n = 1;
+
+INSERT INTO stacks_helper(dogs, stack_height, last_height, n)
+SELECT t1.dogs || ", " || t2.dogs, t1.stack_height + t2.stack_height, t2.last_height, 4
+FROM stacks_helper AS t1, stacks_helper AS t2 WHERE t1.last_height < t2.last_height AND t1.n = 3 AND t2.n = 1;
 
 CREATE TABLE stacks AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT stacks_helper.dogs, stacks_helper.stack_height FROM stacks_helper
+  WHERE stacks_helper.stack_height > 170 AND stacks_helper.n = 4
+  ORDER BY stacks_helper.stack_height;
+
 
